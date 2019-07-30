0cfd3f54d53f1d21cb978a4fd5d162541f6b4273
AJ <amarchitelli1993@gmail.com>
2019-07-30 17:50:50 -0400

    some log_drink stuff

--- app/cli_actions/log_drink.rb
+++ app/cli_actions/log_drink.rb
@@ -1,17 +1,29 @@
-def all_user_userdrinks
-  userdrinks = UserDrink.find_by user_id: self
+
+
+  @prompt.select("Select one of your recents drinks or log a different drink!")
+
+
+choices = []
+
+def log_a_drink_choices(user)
+  user.five_most_recent_drinks.each do |userdrink|
+    choices << " #{userdrink.amount} oz. - #{Drink.find(userdrink.drink_id.name}"
+  end
+  choices << "Log a Different Drink"
 end
 
+#use array in a prompt including log a new drink
 
-def all_user_drinks
-  all_user_userdrinks.drink_id
-end
+def drink_info_prompt
+  puts "What are you having?"
+        @prompt.collect do
+            key(:name).ask('Drink name?', required: true)
+            key(:description).ask('Description of drink')
+            key(:alcohol_parcentage).ask('What is the alcohol percentage?')
+            key(:amount).ask('What is the amount (oz)')
+        end
+    end
 
-
-def log_drink_or_recent_prompt
-end
-
-
-
-def log drink
+def create_drink
+  
 end
